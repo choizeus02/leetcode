446b4f046c579eb14c485003f3438035e7f4cf1f
choizeus <98094516+choizeus02@users.noreply.github.com>
2022-11-23 15:11:00 +0900

    Time: 18 ms (53.33%), Space: 6.8 MB (100.00%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.c
@@ -0,0 +1,43 @@
+#include <string.h>
+int partitionString(char * s){
+    
+    int len = strlen(s);
+    int count=0;
+    for(int i=0; i<len; i++){
+        bool arr[130] = {0};
+        while(i < len){
+            if(arr[s[i]]){
+                i--;
+                break;
+            } 
+            else {
+                arr[s[i]] = true;
+                i++;
+            }
+        }
+        count++;
+    }
+    return count;
+    
+    
+
+}
+
+// int partitionString(char * s){
+//     int len = strlen(s);
+//     int ans = 0;
+//     for (int i = 0 ; i < len ; i++){
+//         bool list[123] = {0};
+//         while(i < len){
+//             if (list[s[i]]){
+//                 i--;
+//                 break;
+//             } else {
+//                 list[s[i]] = true;
+//                 i++;
+//             }
+//         }
+//         ans++;
+//     }
+//     return ans;
+// }
