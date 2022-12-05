1e99ca91db45d2ac753d1e3bcbddc0de755557c0
choizeus <98094516+choizeus02@users.noreply.github.com>
2022-12-05 22:29:02 +0900

    Time: 8 ms (55.00%), Space: 6.4 MB (20.00%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.c
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.c
@@ -1,14 +1,57 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+int isValid(char *main, char *other){
+    int i = 0,j,k;
+    
+    while(main[i]){
+        j = 0;
+        k = i;
+        while(main[k] && other[j] && main[k] == other[j]){
+            k++;
+            j++;
+        }
+        if(other[j] == '\0')
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
+int isUnique(char **arr,int len,char *str){
+    int i = 0;
+    
+    while(i < len){
+        if(strcmp(arr[i],str) == 0)
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
 char ** stringMatching(char ** words, int wordsSize, int* returnSize){
-    char **a = malloc(sizeof(char *[wordsSize])), **w = words;
-    int k = 0;
-    for (int i = 0 ; i < wordsSize ; i++)
-        for (int j = i + 1, x, y ; j < wordsSize ; j++)
-                if (w[j] && w[i] && ((x = !!strstr(w[i], w[j])) || (y = !!strstr(w[j], w[i])))) {
-                    a[k++] = x ? w[j] : w[i];
-                    x ? w[j] = NULL : (w[i] = NULL);
+    int i,j,index = 0;;
+    char **arr;
+    arr = (char **)malloc(sizeof(char *) * (wordsSize-1));
+    
+    for(i = 0; i < wordsSize-1; i++)
+        arr[i] = (char *)malloc(sizeof(char) * 31);
+    
+    i = 0;
+    while(i < wordsSize){
+        j = 0;
+        while(j < wordsSize){
+            while(j < wordsSize && strlen(words[i]) <= strlen(words[j]))
+                j++;
+            if(j < wordsSize && isValid(words[i],words[j])){
+                if(index != 0){
+                    if(isUnique(arr,index,words[j]))
+                        arr[index++] = words[j];       
                 }
-    return *returnSize = k, a;
+                else
+                    arr[index++] = words[j];
+            }
+            j++;
+        }
+        i++;
+    }
+    *returnSize = index;
+    return arr;
 }
